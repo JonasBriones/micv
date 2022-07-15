28aede6ff10d93454ae3d8af8e5b77980eb7fca8
Jonathan Briones Lavados <jonathan.brioneslavados@cencosud.cl>
2022-07-15 12:52:36 -0400

    modificaciones en el cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
 
 
 import imagen from '../assets/imagen.jpg'
@@ -23,16 +24,16 @@ export const Home = () => {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav mr-auto mt-3">
                         <li className="nav-item">
-                            <a href="#about" className="nav-link"><span data-hover="About">Resumen</span></a>
+                            <a href="#about" className="nav-link"><span data-hover="Resumen">Resumen</span></a>
                         </li>
                         <li className="nav-item">
-                            <a href="#project" className="nav-link"><span data-hover="Projects">Proyectos</span></a>
+                            <a href="#project" className="nav-link"><span data-hover="Proyectos">Proyectos</span></a>
                         </li>
                         <li className="nav-item">
-                            <a href="#contact" className="nav-link"><span data-hover="Contact">Tecnologias</span></a>
+                            <a href="#tech" className="nav-link"><span data-hover="Tecnologias">Tecnologias</span></a>
                         </li>
                         <li className="nav-item">
-                            <a href="#contact" className="nav-link"><span data-hover="Contact">Contacto</span></a>
+                            <a href="#contact" className="nav-link"><span data-hover="Contacto">Contacto</span></a>
                         </li>
                     </ul>
                 </div>
@@ -40,38 +41,82 @@ export const Home = () => {
             </div>
         </nav>
         
-        <section className="about full-screen d-lg-flex justify-content-center align-items-center" id="about">
+        <section className="about full-screen d-lg-flex justify-content-center align-items-center" id="tech">
+            <div className="container h-100">
+                <h1 className="text-center">Featured in:</h1>
+                <div className='row align-items-center h-100"'>
+                    <Carousel>
+                        <Carousel.Item>
+                        <img
+                                        className="d-block"
+                                        src="https://picsum.photos/500/300?img=1"
+                                        alt="First slide"
+                                    />
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
+        </section>
+
+        <section className="contact py-5" id="contact">
             <div className="container">
-                <div className='row'>
-                    <div className="col-lg-7 col-md-12 col-12 d-flex align-items-center">
-                        <div className="about-text">
-                            <small className="small-text">Bienvenido a mi porfolio website!</small>
+                <div className="row">
+                    <div className="col-lg-5 mr-lg-5 col-12">
 
-                            <h1 className="animated animated-text">
-                                <span className="mr-4">Hola, soy</span>
-                                    <div className="animated-info">
-                                        <span className="animated-item">Marvel Sann</span>
-                                        <span className="animated-item">Web Designer</span>
-                                        <span className="animated-item">UI Specialist</span>
-                                    </div>
-                            </h1>
+                    </div>
 
-                            <p>Building a successful product is a challenge. I am highly energetic in user experience design, interfaces and web development.</p>
-                            
-                            <div className="custom-btn-group mt-4">
-                                <a href="#" className="btn mr-lg-2 custom-btn"><i className='uil uil-file-alt'></i> Download Resume</a>
-                                <a href="#contact" className="btn custom-btn custom-btn-bg custom-btn-link">Get a free quote</a>
+                    <div className="col-lg-6 col-12">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className="contact py-5" id="contact">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-5 mr-lg-5 col-12">
+                        <div className='google-ap w-100'>
+                            <div>
+                                <iframe 
+                                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=santiaguo%20centro+(Yo)&amp;t=p&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                                    <a href="https://www.gps.ie/car-satnav-gps/">Sat Navs</a>
+                                </iframe>
                             </div>
-
                         </div>
                     </div>
 
-                    <div className="col-lg-5 col-md-12 col-12">
-                        <div className="about-image svg">
-                            <img src={imagen} className="img-fluid" alt="svg image" />
+                    <div className="col-lg-6 col-12">
+                        <div className="contact-form">
+                            <h2 className="mb-4">¿Interesado en trabajar juntos? Hablemos</h2>
+                            <form>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className="contact py-5" id="contact">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-5 mr-lg-5 col-12">
+                        <div className='google-ap w-100'>
+                            <div>
+                                <iframe 
+                                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=santiaguo%20centro+(Yo)&amp;t=p&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                                    <a href="https://www.gps.ie/car-satnav-gps/">Sat Navs</a>
+                                </iframe>
+                            </div>
                         </div>
                     </div>
 
+                    <div className="col-lg-6 col-12">
+                        <div className="contact-form">
+                            <h2 className="mb-4">¿Interesado en trabajar juntos? Hablemos</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
