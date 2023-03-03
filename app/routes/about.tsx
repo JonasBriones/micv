07de2502b373803b1fbac5eddaf9a161dd561169
Jonas <jonathan.brioneslavados@cencosud.cl>
2023-03-03 14:21:57 -0300

    modificamos el lint

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
-
 export default function About() {
-  return (
-    <React.Fragment>
-      <h1>test</h1>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<h1>test</h1>
+		</React.Fragment>
+	);
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
-
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  return (
-    <React.Fragment>
-      <h1>hola</h1>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<h1>hola</h1>
+		</React.Fragment>
+	);
 }
