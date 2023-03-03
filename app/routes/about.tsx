6ef8ddfd8d757c03e36b56b02327733e061ef23b
Jonas <jonathan.brioneslavados@cencosud.cl>
2023-03-03 15:38:09 -0300

    modificaciones en los despliegues

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const About: React.FC = () => {
 	return (
 		<React.Fragment>
-			<h1>test</h1>
+			<h1>Adios</h1>
 		</React.Fragment>
 	);
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,13 @@
+import { Box, Typography } from '@mui/material';
 import * as React from 'react';
 
 // https://remix.run/guides/routing#index-routes
 const Index: React.FC = () => {
 	return (
 		<React.Fragment>
-			<h1>hola</h1>
+			<Box>
+				<Typography variant='h1'>Bienvenidos</Typography>
+			</Box>
 		</React.Fragment>
 	);
 };
